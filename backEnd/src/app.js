54c85e2b210a6a641ab59f960e4fd4a309afb479
Yanshi Agarwal <aparul742@gmail.com>
2026-04-02 16:54:07 +0530

    add cookie

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const app = express();
 const todoRoutes = require('./routes/todo.route');
+const cookieParser = require('cookie-parser');
+// const session = require('express-session');  
 
 app.use(express.json());
-app.use('/api/todos', todoRoutes);
 
+
+app.use(cookieParser());
+
+app.get("/set-cookie", (req, res) => {
+    res.cookie("name","user-1");
+    res.send("Cookie has been set");
+});
+
+app.get("/get-cookie", (req, res) => {
+    res.json(req.cookies);
+});
+
+// app.use(session({
+// secret: "my-secret-key",
+// resave: false,
+// saveUninitialized: true,
+// cookie: { secure: false }
+// }));
+
+app.use('/api/todos', todoRoutes);
 module.exports = app;
